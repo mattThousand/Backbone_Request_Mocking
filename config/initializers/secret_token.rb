999c672588d4273e5f304485dca32360a446c0b4
mattThousand <matt.d.buckley1212@gmail.com>
2013-10-28 08:46:04 -0400

    Setup backbone infrastructure

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BackboneTestingExample::Application.config.secret_token = 'ea420e6819d66ae7701fdbafed31302a4dc2175a562912b37c36688608f60cdd35030bc4dc7af74a7f8f3ec15794b5f4354ffd433b2c77ba3bf034a0991b4eaa'
